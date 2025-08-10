21ba149d7578831e7b1e4b1ad63cdfff9dcd5965
Jason <jason.debottis@gmail.com>
2025-08-10 16:08:37 -0400

    WIP

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
+import { MatMenuTrigger } from '@angular/material/menu';
 import { QuoteDialogComponent } from '../quote-dialog/quote-dialog.component';
 import { ContactDialogComponent } from '../contact-dialog/contact-dialog.component';
 
@@ -31,12 +32,23 @@ export class HomeComponent {
       ],
     },
     { name: 'Quote', route: '/quote', icon: 'request_quote' },
-    { name: 'About', route: '/about', icon: 'info' },
+    {
+      name: 'About',
+      route: '/about',
+      icon: 'info',
+      submenu: [
+        { name: 'About Lets Roll', route: '/about/lets-roll', icon: 'history' },
+        { name: 'Safety', route: '/about/safety', icon: 'shield' },
+        { name: 'Leadership', route: '/about/leadership', icon: 'leaderboard' },
+      ],
+    },
     { name: 'Contact', route: '/contact', icon: 'contact_mail' },
   ];
 
   isMobileMenuOpen = false;
   openSubmenu: string | null = null;
+  hoveredMenuItem: string | null = null;
+  currentOpenTrigger: MatMenuTrigger | null = null;
 
   constructor(private router: Router, private dialog: MatDialog) {}
 
@@ -51,17 +63,82 @@ export class HomeComponent {
 
   toggleSubmenu(menuName: string) {
     this.openSubmenu = this.openSubmenu === menuName ? null : menuName;
+    console.log('Toggled submenu:', this.openSubmenu);
+  }
+
+  showSubmenu(item: any) {
+    if (item.submenu.length > 0) {
+      this.openSubmenu = item.name;
+    }
+  }
+  hideSubmenu() {
+    this.openSubmenu = null;
   }
 
   hasSubmenu(item: any): boolean {
     return item.submenu && item.submenu.length > 0;
   }
 
-  onMenuItemClick(item: any) {
+  onMenuItemHover(item: any, trigger: MatMenuTrigger | null) {
+    if (this.hasSubmenu(item) && trigger) {
+      // Close any previously open submenu if hovering over a different item with submenu
+      if (this.currentOpenTrigger && this.currentOpenTrigger !== trigger) {
+        this.currentOpenTrigger.closeMenu();
+      }
+
+      // Open the new submenu
+      this.hoveredMenuItem = item.name;
+      this.currentOpenTrigger = trigger;
+      if (!trigger.menuOpen) {
+        trigger.openMenu();
+      }
+    } else if (!this.hasSubmenu(item)) {
+      // If hovering over a menu item without submenu, close any open submenu
+      if (this.currentOpenTrigger) {
+        this.currentOpenTrigger.closeMenu();
+        this.currentOpenTrigger = null;
+      }
+      this.hoveredMenuItem = null;
+    }
+  }
+
+  onMenuItemLeave(trigger: MatMenuTrigger) {
+    // Don't close the submenu when leaving the menu item
+    // It will only close when hovering over another menu item or leaving the menu area entirely
+  }
+
+  onSubmenuHover(trigger: MatMenuTrigger) {
+    // Keep the submenu open when hovering over it
+    this.currentOpenTrigger = trigger;
+  }
+
+  onSubmenuLeave(trigger: MatMenuTrigger) {
+    // Close the submenu when the user leaves the submenu area
+    if (this.currentOpenTrigger === trigger && trigger.menuOpen) {
+      trigger.closeMenu();
+      this.currentOpenTrigger = null;
+      this.hoveredMenuItem = null;
+    }
+  }
+
+  onMenuAreaLeave() {
+    // Close any open submenu when leaving the entire menu area
+    if (this.currentOpenTrigger && this.currentOpenTrigger.menuOpen) {
+      this.currentOpenTrigger.closeMenu();
+      this.currentOpenTrigger = null;
+    }
+    this.hoveredMenuItem = null;
+  }
+
+  shouldShowSubmenu(item: any): boolean {
+    return this.hasSubmenu(item) && this.hoveredMenuItem === item.name;
+  }
+
+  onMenuItemClick(item: any, isMobileClick: boolean = false) {
     console.log('Menu item clicked:', item.name);
 
-    // If item has submenu, toggle it instead of navigating
-    if (this.hasSubmenu(item)) {
+    // If item has submenu and this is a mobile click, toggle it instead of navigating
+    if (this.hasSubmenu(item) && (isMobileClick || this.isMobileMenuOpen)) {
       this.toggleSubmenu(item.name);
       return;
     }
